43f52eb05526a4b85f8fd6b7e7239909457c13e5
Kacper Roszczak <k.roszczak@interia.pl>
2020-01-09 12:08:18 +0100

    code refactor

--- main.js
+++ main.js
@@ -1,57 +1,47 @@
 
-let move = ['',''];
-const player1 = [65, 83, 68];
-const player2 = [74, 75, 76];
+let players = [player1 = {pmoves: [65, 83, 68], move : '' },  player2 = {pmoves: [74, 75, 76], move: ''}];
 const moves = [65, 75, 68, 74, 83, 76, 65];
 let display = document.querySelectorAll('.display');
 
 document.addEventListener('keydown', (b) =>{
     
-    if(player1.includes(b.keyCode) && move[0] === '')
-    {   
-        console.log("gracz UNO");
-        move[0] = b.keyCode;
+    if(player1.pmoves.includes(b.keyCode) && player1.move === ''){   
+        player1.move = b.keyCode;
         battle();
     }
-    else if(player2.includes(b.keyCode) && move[1] === ''){
+    else if(player2.pmoves.includes(b.keyCode) && player2.move === ''){
     
-        console.log("gracz DOS");
-        move[1] = b.keyCode;
+        player2.move = b.keyCode;
         battle();     
     }
 
 })
 
 function battle(){
-    if((move[0] !== '') && (move[1] !== '')){
-        if(moves[moves.findIndex((e) => {return e === move[0];}) + 1] === move[1]){
-            console.log("lewy wygrywa");
+
+    if(( player1.move !== '') && (player2.move !== '')){
+        
+        if(moves[moves.findIndex((move) => {return move === player1.move;}) + 1] === player2.move){
             document.querySelectorAll('.counter')[0].innerHTML += ".";
             document.querySelector('#mark').innerHTML = ">";
         }
-        else if(moves[moves.findIndex((e) => {return e === move[1];}) + 1] === move[0]){
-            console.log("prawy wygrywa");
+        else if(moves[moves.findIndex((move) => {return move === player2.move;}) + 1] === player1.move){
             document.querySelectorAll(".counter")[1].innerHTML += ".";
             document.querySelector('#mark').innerHTML = "<";
         }
-        else {console.log("DRAW"); document.querySelector('#mark').innerHTML = "=";}
-
+        else {
+            document.querySelector('#mark').innerHTML = "=";
+        }
             
         display.forEach( (item, index) =>{
-            x = `<img src = "images/${(move[index] == 65 || move[index] == 74) ? "paper.png" : ((move[index] == 75 || move[index] == 83) ? "rock.png" : "scissors.png")}" />`;
+            x = `<img src = "images/${(players[index].move == 65 || players[index].move  == 74) ? "paper.png" : ((players[index].move  == 75 || players[index].move  == 83) ? "rock.png" : "scissors.png")}" />`;
 
             display[index].innerHTML = x;
         });
-        move = ['',''];
+        player1.move = '';
+        player2.move = '';
     }
 }
-
-//a 65 - papier1
-//k 75 - kamien2
-//d 68 - nozyce1
-//j 74 - papier2
-//s 83 - kamien1
-//l 76 - nozyce2
 
 
 
