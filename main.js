7375a5bd3ee61d838c33a76f308a92820685f715
Kacper Roszczak <k.roszczak@interia.pl>
2020-01-09 19:15:53 +0100

    Win Alert

--- main.js
+++ main.js
@@ -21,13 +21,11 @@ document.addEventListener('keydown', (b) =>{
     
     if(player1.pmoves.includes(b.keyCode) && player1.move === ''){  
 
-        display[0].style.animation ="";
         player1.move = b.keyCode;
         battle();
     }
     else if(player2.pmoves.includes(b.keyCode) && player2.move === ''){
 
-        display[1].style.animation ="";
         player2.move = b.keyCode;
         battle();     
     }else if(b.keyCode === 82) reset();
@@ -37,55 +35,50 @@ document.addEventListener('keydown', (b) =>{
 
 function battle(){
 
-    if(( player1.move !== '') && (player2.move !== '')){
+    if((player1.move !== '') && (player2.move !== '')){
 
         if(moves[moves.findIndex((move) => {return move === player1.move;}) + 1] === player2.move){
+
             player1.pts++;
             counter[0].innerHTML += ".";
             document.querySelector('#mark').innerHTML = ">";
-            if(player1.pts == 5) win(player1.name);
         }
         else if(moves[moves.findIndex((move) => {return move === player2.move;}) + 1] === player1.move){
+
             player2.pts++;
             counter[1].innerHTML += ".";
             document.querySelector('#mark').innerHTML = "<";
-            
         }
-        else {
-            document.querySelector('#mark').innerHTML = "=";
-        }
+        else {  document.querySelector('#mark').innerHTML = "=";}
         
-        document.querySelector('#mark').classList.add("dissapper");
+
         display.forEach( (item, index) =>{
             x = `<img src = "images/${(players[index].move == 65 || players[index].move  == 74) ? "paper.png" : ((players[index].move  == 75 || players[index].move  == 83) ? "rock.png" : "scissors.png")}" />`;
-            
             display[index].innerHTML = x;
         });
-        if(player1.pts == 5){ 
-            win(player1.name);}
-        else if(player2.pts == 5){
-            win(player2.name);}
-        else{
-            player1.move = ''; player2.move = '';}
+
+        if(player1.pts == 5){ win(player1.name);}
+        else if(player2.pts == 5){win(player2.name);}
+        else{ player1.move = ''; player2.move = '';}
         
     }
 }
 
 function win(x){
-
+    document.querySelector('#mark ').style.fontSize = `3.4rem` 
+    document.querySelector('#mark ').innerHTML = `${x} wygrywa` 
 }
 
 function reset(){
 
     document.querySelector('#mark').innerHTML = '';
+    document.querySelector('#mark ').style.fontSize = `30rem` 
     counter.forEach((item, i) => {
         display[i].innerHTML = cntnt[i];
         counter[i].innerHTML = '';
         players[i].move = '';
         players[i].pts = 0;
     })
-
-
 }
 
 
